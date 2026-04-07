340c170448629c8f56ee9843fc7b809f33b5b868
Mangal Mandor <mangal.mandor13102003@therapidhire.com>
2026-04-07 17:11:15 +0530

    Update authSlice.js

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -28,6 +28,7 @@ export const logoutUser = createAsyncThunk('auth/logout', async () => {
     await api.post('/auth/logout');
     localStorage.removeItem('token');
     localStorage.removeItem('user');
+    localStorage.removeItem('cartItems');
     disconnectSocket();
     return null;
 });
