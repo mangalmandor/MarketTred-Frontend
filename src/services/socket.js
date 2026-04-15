25dc2e3850df7acd27d8617878ed89609027b03a
Mangal Mandor <mangal.mandor13102003@therapidhire.com>
2026-04-15 17:03:37 +0530

    Update versal cors socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -6,6 +6,7 @@ console.log(SOCKET_URL);
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   transports: ['websocket', 'polling'],
+  withCredentials: true,
 });
 
 export const connectSocket = (token) => {
